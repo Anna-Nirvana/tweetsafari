211645458c1a3ec637dfcd3d145c4b6ea459ee23
Anna <annmarcam@gmail.com>
2021-03-22 18:14:31 -0700

    remove notes

--- script.js
+++ script.js
@@ -7,19 +7,3 @@ style: 'mapbox://styles/mapbox/streets-v11', // style URL
 center: [-74.5, 40], // starting position [lng, lat]
 zoom: 9 // starting zoom
 });
-
-/* Leaflet template - set geo coordinates for start view:
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
-
-    //Mapbox
-    L.mapbox.styleLayer('mapbox://styles/YOUR_USERNAME/YOUR_STYLE_ID').addTo(map);
-
-//Leaflet reference URL for Mapbox: https://api.mapbox.com/styles/v1/nu-ryip/ckmf5uxn1dao617p8ggx2dgui.html?fresh=true&title=view&access_token=pk.eyJ1IjoibnUtcnlpcCIsImEiOiJja201Nzdia2swYmh4Mm9xYmJ5cDk4aHVrIn0.vB38tUtuXc1dV28hICwO1A
